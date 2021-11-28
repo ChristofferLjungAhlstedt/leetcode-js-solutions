be7935394b675b8a00978ecdec99e64985574ee0
YureTarded <christoffer.ljungahlstedt@gmail.com>
2021-11-28 23:25:43 +0100

    The list is linked, but does not have the right return type for leet

--- mergeKLists/app.js
+++ mergeKLists/app.js
@@ -1,15 +1,51 @@
 function sortList(lists) { // THis works, But is not a linked list
     let bigList = [];    
     for (let i = 0; i < lists.length; i++) {
-        if (typeof(lists[i]) == 'number') {
-            bigList.push(lists[i])
-        }
-        else if (typeof(lists[i]) == 'object') {
+        if (typeof(lists[i]) == 'object') {
             let currList = lists[i];
             for (let j = 0; j < currList.length; j++) {
                 bigList.push(currList[j]);
             }
+            continue
         }
+        bigList.push(lists[i])
     }
-    return bigList.sort();
+    bigList.sort();
+    const sortedLinked = LinkedListFactory()
+    for (let i = 0; i < bigList; i++) {
+        sortedLinked.append(bigList[i]);
+    }
+    return sortedLinked;
 }
+
+function LinkedListFactory() {
+    let head = null;
+    let length = 0;
+  
+    return {
+      append
+    };
+  
+    function append(element) {
+      const node = {
+        element,
+        next: null,
+      };
+  
+      if (head === null) {
+        head = node
+      } else {
+        let currentNode = head;
+  
+        while (currentNode.next !== null) {
+          currentNode = currentNode.next;
+        }
+  
+        currentNode.next = node;
+      }
+  
+      length++;
+    }
+}
+
+console.log(sortList([[1,4,5],[1,3,4],[2,6]]));
